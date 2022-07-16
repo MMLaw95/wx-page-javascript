fabef090c87c6b389fe8b4d8c038fccba001c4de
MMLaw95 <103419849+MMLaw95@users.noreply.github.com>
2022-07-16 04:24:25 -0500

    Testing out new format

--- src/app.js
+++ src/app.js
@@ -45,7 +45,7 @@ function displayForecast(response) {
 
   let forecastElement = document.querySelector("#forecast");
 
-  let forecastHTML = `<div class="row group-days">`;
+  let forecastHTML = `<div class="row group-days px-md-5">`;
   forecast.forEach(function (forecastDay, index) {
     if (index < 6) {
       forecastHTML =
