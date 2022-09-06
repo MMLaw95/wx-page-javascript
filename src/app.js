64d2c41e427cae3e7533cba20d43efee23a1a168
MMLaw95 <103419849+MMLaw95@users.noreply.github.com>
2022-09-06 12:55:28 -0500

    Update api

--- src/app.js
+++ src/app.js
@@ -86,7 +86,7 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "7319e499982a9c6f662ba9e3a5ccdf2d";
+  let apiKey = "a43564c91a6c605aeb564c9ed02e3858";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayForecast);
 }
@@ -120,7 +120,7 @@ function displayWeatherConditions(response) {
 }
 
 function search(city) {
-  let apiKey = "7319e499982a9c6f662ba9e3a5ccdf2d";
+  let apiKey = "a43564c91a6c605aeb564c9ed02e3858";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayWeatherConditions);
 }
@@ -132,7 +132,7 @@ function handleSubmit(event) {
 }
 
 function searchLocation(position) {
-  let apiKey = "7319e499982a9c6f662ba9e3a5ccdf2d";
+  let apiKey = "a43564c91a6c605aeb564c9ed02e3858";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayWeatherConditions);
 }
@@ -183,7 +183,7 @@ clickLondon.addEventListener("click", displayLondon);
 
 function lookUpLondon(event) {
   let city = "London";
-  let apiKey = "7319e499982a9c6f662ba9e3a5ccdf2d";
+  let apiKey = "a43564c91a6c605aeb564c9ed02e3858";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayWeatherConditions);
 }
@@ -199,7 +199,7 @@ clickTokyo.addEventListener("click", displayTokyo);
 
 function lookUpTokyo(event) {
   let city = "Tokyo";
-  let apiKey = "7319e499982a9c6f662ba9e3a5ccdf2d";
+  let apiKey = "a43564c91a6c605aeb564c9ed02e3858";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayWeatherConditions);
 }
@@ -215,7 +215,7 @@ clickNewYork.addEventListener("click", displayNewYork);
 
 function lookUpNewYork(event) {
   let city = "New York";
-  let apiKey = "7319e499982a9c6f662ba9e3a5ccdf2d";
+  let apiKey = "a43564c91a6c605aeb564c9ed02e3858";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayWeatherConditions);
 }
@@ -231,7 +231,7 @@ clickReykjavik.addEventListener("click", displayReykjavik);
 
 function lookUpReykjavik(event) {
   let city = "Reykjavik";
-  let apiKey = "7319e499982a9c6f662ba9e3a5ccdf2d";
+  let apiKey = "a43564c91a6c605aeb564c9ed02e3858";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayWeatherConditions);
 }
@@ -247,7 +247,7 @@ clickLagos.addEventListener("click", displayLagos);
 
 function lookUpLagos(event) {
   let city = "Lagos";
-  let apiKey = "7319e499982a9c6f662ba9e3a5ccdf2d";
+  let apiKey = "a43564c91a6c605aeb564c9ed02e3858";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayWeatherConditions);
 }
